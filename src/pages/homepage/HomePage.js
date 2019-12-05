81c31d18422d63917d5e25d5812a669ea5201b77
chiranz <chiranpoudyal@gmail.com>
2019-12-05 15:58:56 +0530

    Broken homepage into smaller components

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,41 +1,11 @@
 import React from "react";
 import "./homepage.style.scss";
+import DirectoryMenu from "../../components/directoryMenu/DirectoryMenu";
 
 export default function HomePage() {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-      </div>
+      <DirectoryMenu />
     </div>
   );
 }
