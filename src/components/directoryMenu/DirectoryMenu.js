8123fa7e80964afbc6b2670e1f46f58ca268a76f
chiranz <chiranpoudyal@gmail.com>
2019-12-06 12:30:29 +0530

    react router dom setup and navigate using withRouter

--- src/components/directoryMenu/DirectoryMenu.js
+++ src/components/directoryMenu/DirectoryMenu.js
@@ -39,8 +39,8 @@ export default function DirectoryMenu() {
   ]);
   return (
     <div className="directory-menu">
-      {sections.map(({ id, title, imageUrl, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./homepage.style.scss";
 import DirectoryMenu from "../../components/directoryMenu/DirectoryMenu";
 
-export default function HomePage() {
+export default function HomePage(props) {
+  console.log(props);
   return (
     <div className="homepage">
       <DirectoryMenu />
