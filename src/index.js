a5f6956a0eef4aea581dbd78b07aaca8bf467993
chiranz <chiranpoudyal@gmail.com>
2020-02-26 11:13:09 +0530

    storing data using redux-persist in local storage

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
 import App from "./App";
-import store from "./store";
+import { store, persistor } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
 
--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { persistStore } from "redux-persist";
 
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(rootReducer, devTools);
+export const store = createStore(rootReducer, devTools);
 
-export default store;
+export const persistor = persistStore(store);
