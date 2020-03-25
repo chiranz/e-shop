59e1496ae1915da5e42c2b0273c754a0cf63459b
chiranz <chiranpoudyal@gmail.com>
2020-03-25 11:28:13 +0530

    Stripe setup

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./checkoutPage.style.scss";
 import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/checkoutItem/CheckoutItem";
+import StripeButton from "../../components/stripe-button/StripeButton";
 
 export default function CheckoutPage() {
   const { cartItems } = useSelector(state => state.cart);
@@ -34,6 +35,16 @@ export default function CheckoutPage() {
       <div className="total">
         <span>Total: ${cartTotal}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242-4242-4242-4242
+        <br />
+        Exp: 01/20Next
+        <br />
+        CVV: 123
+      </div>
+      <StripeButton price={cartTotal} />
     </div>
   );
 }
