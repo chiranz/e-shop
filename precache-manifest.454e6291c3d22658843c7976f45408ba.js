1995967c3402da270a36c4386802f302de788b8b
chiranz <chiranpoudyal@gmail.com>
2020-04-15 10:39:33 +0530

    Updates

---
+++ precache-manifest.454e6291c3d22658843c7976f45408ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa63d033b311bb0c489424bc81a6d26",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "a342a60b3d01230d252a",
+    "url": "/e-shop/static/css/main.c5d6fe67.chunk.css"
+  },
+  {
+    "revision": "b6034a0b5e2bc54cd5b1",
+    "url": "/e-shop/static/js/2.518685f3.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/e-shop/static/js/2.518685f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a342a60b3d01230d252a",
+    "url": "/e-shop/static/js/main.a7efe80f.chunk.js"
+  },
+  {
+    "revision": "e3a21483ff05fe32da1d",
+    "url": "/e-shop/static/js/runtime-main.2681c94e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "97edb47c0c06f1f21149691922881d18",
+    "url": "/e-shop/static/media/shopping-bag.97edb47c.svg"
+  }
+]);
