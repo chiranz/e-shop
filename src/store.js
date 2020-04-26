88d5c171dee6e3b78bb1bcef85aae657e15bc54e
chiranz <chiranpoudyal@gmail.com>
2020-04-26 20:00:57 +0530

    removed gh pages

--- src/store.js
+++ src/store.js
@@ -3,14 +3,14 @@ import rootReducer from "./reducers";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const middlewares = [thunk];
 
 export const store = createStore(
   rootReducer,
-  compose(applyMiddleware(...middlewares), devTools)
+  compose(applyMiddleware(...middlewares))
 );
 
 export const persistor = persistStore(store);
